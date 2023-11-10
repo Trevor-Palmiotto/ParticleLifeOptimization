98d444a2ed2453dac6fa1e1ac06ca03ca9ff3cce
Trevor-Palmiotto <ssztrevor@gmail.com>
2023-11-09 22:14:48 -0500

    getNearestNeighbors - finished proved using visualization

--- particleLifeClass.js
+++ particleLifeClass.js
@@ -44,9 +44,10 @@ function getNearestNeighbors(ht, wd) {
     var NN = [];
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-            // 
+            NN.push(Hs[i] * w + Ws[j]);
         }
     }
+    return NN;
 }
 
 window.addEventListener("click", function() {
