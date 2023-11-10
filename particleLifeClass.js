be098d1b9efd8dad2330bcf17b2aaa3c1b47541b
Trevor-Palmiotto <ssztrevor@gmail.com>
2023-11-09 22:10:01 -0500

    last comment meant to say found height/width nearest neighbors using same method for wrapping: (x + w) % w

--- particleLifeClass.js
+++ particleLifeClass.js
@@ -44,7 +44,7 @@ function getNearestNeighbors(ht, wd) {
     var NN = [];
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-            
+            // 
         }
     }
 }
