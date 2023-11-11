af5dafc1f30566c71ba1ea315aaf24532547dc38
Trevor-Palmiotto <ssztrevor@gmail.com>
2023-11-10 21:50:37 -0500

    *Arrays fully set for 3D including wrapping and such. Now need to consider OOP and how to handle the +/- using the abnormalized *Class version. First figure out how to do 3D grid partitioning using the same model of FOV

--- particleLifeArrays.js
+++ particleLifeArrays.js
@@ -9,7 +9,7 @@ const dt = 0.02;
 const frictionHalfLife = 0.040;
 const m = 6;
 const matrix = makeRandomMatrix();
-const forceFactor = 10;
+const forceFactor = 5;
 
 const frictionFactor = Math.pow(0.5, dt / frictionHalfLife);
 
@@ -37,6 +37,9 @@ for (let i = 0; i < n; i++) {
     positionsX[i] = Math.random() * 2 - 1;
     positionsY[i] = Math.random() * 2 - 1;
     positionsZ[i] = Math.random() * 2 - 1;
+    // positionsX[i] = Math.random() - 0.5;
+    // positionsY[i] = Math.random() - 0.5;
+    // positionsZ[i] = Math.random() - 0.5;
     velocitiesX[i] = 0;
     velocitiesY[i] = 0;
     velocitiesZ[i] = 0;
@@ -66,15 +69,15 @@ function updateParticles() {
             var rx = positionsX[j] - positionsX[i];
             var ry = positionsY[j] - positionsY[i];
             var rz = positionsZ[j] - positionsZ[i];
-            // if (Math.abs(rx) > 0.5) {
-            //     rx = (1 - Math.abs(rx)) * -rx / Math.abs(rx);
-            // }
-            // if (Math.abs(ry) > 0.5) {
-            //     ry = (1 - Math.abs(ry)) * -ry / Math.abs(ry);
-            // }
-            // if (Math.abs(rz) > 0.5) {
-            //     ry = (1 - Math.abs(rz)) * -(rz) / Math.abs(rz);
-            // }
+            if (Math.abs(rx) > 1) {
+                rx = (2 - Math.abs(rx)) * -rx / Math.abs(rx);
+            }
+            if (Math.abs(ry) > 1) {
+                ry = (2 - Math.abs(ry)) * -ry / Math.abs(ry);
+            }
+            if (Math.abs(rz) > 1) {
+                rz = (2 - Math.abs(rz)) * -rz / Math.abs(rz);
+            }
             const r = Math.sqrt(rx*rx + ry*ry + rz*rz);
             if (r > 0 && r < rMax) {
                 const f = force(r / rMax, matrix[colors[i]][colors[j]]);
@@ -87,14 +90,23 @@ function updateParticles() {
         totalForceX *= rMax * forceFactor;
         totalForceY *= rMax * forceFactor;
         totalForceZ *= rMax * forceFactor;
-    
+        // totalForceX *= rMax * forceFactor/2;
+        // totalForceY *= rMax * forceFactor/2;
+        // totalForceZ *= rMax * forceFactor/2;
+
         velocitiesX[i] *= frictionFactor;
         velocitiesY[i] *= frictionFactor;
         velocitiesZ[i] *= frictionFactor;
+        // velocitiesX[i] *= frictionFactor / 2;
+        // velocitiesY[i] *= frictionFactor / 2;
+        // velocitiesZ[i] *= frictionFactor / 2;
 
         velocitiesX[i] += totalForceX * dt;
         velocitiesY[i] += totalForceY * dt;
         velocitiesZ[i] += totalForceZ * dt;
+        // velocitiesX[i] += totalForceX * dt/2;
+        // velocitiesY[i] += totalForceY * dt/2;
+        // velocitiesZ[i] += totalForceZ * dt/2;
     }
 
     //update positions
@@ -102,9 +114,9 @@ function updateParticles() {
         positionsX[i] += velocitiesX[i] * dt;
         positionsY[i] += velocitiesY[i] * dt;
         positionsZ[i] += velocitiesZ[i] * dt;
-        // positionsX[i] = (positionsX[i] + 1) % 1
-        // positionsY[i] = (positionsY[i] + 1) % 1
-        // positionsZ[i] = (positionsZ[i] + 1) % 1
+        positionsX[i] = ((Math.abs(positionsX[i]) + 1) % 2 - 1) * positionsX[i] / Math.abs(positionsX[i])
+        positionsY[i] = ((Math.abs(positionsY[i]) + 1) % 2 - 1) * positionsY[i] / Math.abs(positionsY[i])
+        positionsZ[i] = ((Math.abs(positionsZ[i]) + 1) % 2 - 1) * positionsZ[i] / Math.abs(positionsZ[i])
     }
 }
 
@@ -115,15 +127,41 @@ function loop() {
     // draw particles
     c.fillStyle = "black";
     c.fillRect(0, 0, canvas.width, canvas.height);
-
+    c.beginPath();
+    c.moveTo(0, canvas.height);
+    c.lineTo(canvas.width/3, canvas.height*2/3);
+    c.strokeStyle = 'white';
+    c.lineTo(canvas.width*2/3, canvas.height*2/3)
+    c.lineTo(canvas.width*2/3, canvas.height/3);
+    c.lineTo(canvas.width/3, canvas.height/3);
+    c.lineTo(canvas.width/3, canvas.height*2/3);
+    c.stroke();
+    c.beginPath();
+    c.moveTo(0,0);
+    c.lineTo(canvas.width/3, canvas.height/3);
+    c.stroke();
+    c.beginPath();
+    c.moveTo(canvas.width,0);
+    c.lineTo(canvas.width*2/3, canvas.height/3);
+    c.stroke();
+    c.beginPath();
+    c.moveTo(canvas.width,canvas.height);
+    c.lineTo(canvas.width*2/3, canvas.height*2/3);
+    c.stroke();
     for (let i = 0; i < n; i++) {
+
         c.beginPath();
         const f = 1 / (positionsZ[i] + 2)
         const screenX = (f * positionsX[i] + 1) * 0.5 * canvas.width;        
         const screenY = (f * positionsY[i] + 1) * 0.5 * canvas.height;
-        c.arc(screenX, screenY, 2, 0, 2 * Math.PI);
+        // const f = 1 / (positionsZ[i] + 1)
+        // const screenX = (f * positionsX[i] + 1) * 0.5 * canvas.width;        
+        // const screenY = (f * positionsY[i] + 1) * 0.5 * canvas.height;
+        c.arc(screenX, screenY, 3 * (1 - ((positionsZ[i] + 1) * 0.5)), 0, 2 * Math.PI);
         c.fillStyle = `hsl(${360 * (colors[i] / m)}, 100%, 50%)`;
         c.fill();
+
+
     }
 
     requestAnimationFrame(loop);
