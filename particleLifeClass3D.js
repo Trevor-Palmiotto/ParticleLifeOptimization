4d65371dd9ff88ba105950d2ea71e96e7815d3da
Trevor-Palmiotto <ssztrevor@gmail.com>
2023-11-12 21:23:50 -0500

    check box for showing grid on/off. Ready for 3D particle life that is independent of l*w*h and has grid partitioning and boundary conditions!

--- particleLifeClass3D.js
+++ particleLifeClass3D.js
@@ -2,64 +2,101 @@ var canvas = document.getElementById('myCanvas');
 const rMax = 200;
 canvas.width = Math.floor(innerWidth / rMax) * rMax;
 canvas.height = Math.floor(innerHeight / rMax) * rMax;
-// console.zog(canvas.width);
-// console.zog(canvas.height);
 var c = canvas.getContext('2d');
-
+var boxDrawn = false;
 const d = 1; // represents distance from the viewport of perspective
 
-var w = canvas.width;
-var h = canvas.height;
-var l = w;
+var w;
+var h;
+var l; 
+var numW;
+var numH;
+var numL;
 
-var numW = w / rMax;
-var numH = h / rMax;
-var numL = Math.min(numH, numW);
-console.log(numW);
-console.log(numH);
-console.log(numL);
+function reDim(){
+    canvas.width = Math.floor(innerWidth / rMax) * rMax;
+    canvas.height = Math.floor(innerHeight / rMax) * rMax;
+    w = canvas.width;
+    h = canvas.height;
+    l = w;
+    numW = w / rMax;
+    numH = h / rMax;
+    numL = Math.min(numH, numW);
 
-
+}
+reDim();
 // This needs to be scaled
 
 const Point = function(x, y, l) {this.x = x; this.y = y; this.z = l / Math.min(h,w) + d};
-
-var boxPoints = [
-    new Point(-w, -h, 0),
-    new Point(w, -h, 0),
-    new Point(w, h, 0),
-    new Point(-w, h, 0),
-    new Point(-w, -h, l),
-    new Point(w, -h, l),
-    new Point(w, h, l),
-    new Point(-w, h, l),
-];
-
-var boxEdges = [
-    [0, 1], [1, 2], [2, 3], [3, 0],
-    [0, 4], [1, 5], [2, 6], [3, 7],
-    [4, 5], [5, 6], [6, 7], [7, 4]
-];
+var boxPoints = [];
+var boxEdges = [];
+function updateBox() {
+    boxPoints = [
+        new Point(-w, -h, 0),
+        new Point(w, -h, 0),
+        new Point(w, h, 0),
+        new Point(-w, h, 0),
+        new Point(-w, -h, l),
+        new Point(w, -h, l),
+        new Point(w, h, l),
+        new Point(-w, h, l),
+    ];
+    
+    boxEdges = [
+        [0, 1], [1, 2], [2, 3], [3, 0],
+        [0, 4], [1, 5], [2, 6], [3, 7],
+        [4, 5], [5, 6], [6, 7], [7, 4]
+    ];
+}
+updateBox();
 
 function drawBox() {
+    c.strokeStyle = 'white';
+    c.lineWidth = 1;
     for (let i = 0; i < boxEdges.length; i++) {
         c.beginPath();
         c.moveTo(computeX(boxPoints[boxEdges[i][0]]), computeY(boxPoints[boxEdges[i][0]]));
         c.lineTo(computeX(boxPoints[boxEdges[i][1]]), computeY(boxPoints[boxEdges[i][1]]));
-        c.strokeStyle = 'red';
         c.stroke();
     }
 }
 
-function drawPoints() {
-    for (let i = -numH; i < numH + 1; i++) {
-            c.fillStyle = `hsl(${360 * i / numH}, 100%, 50%)`
-            for (let j = -numW; j < numW + 1; j++) {
-            for (let k = 0; k < 2*numL +1; k++) {
-                var point = new Point(w / numW * i * (w/h), h / numH * j* (h/w), l / numL /2 * k)
+function drawGridBox() {
+    c.strokeStyle = 'red';
+    c.fillStyle = 'red';    
+    for (let j = -numW; j < numW+1; j++) {
+        for (let i = -numH; i < numH+1; i++) {
+
+        for (let k = 0; k < 2*numL +1; k++) {
+                var pointActual = new Point(w / numW * i * (w/h), h / numH * j* (h/w), l / numL /2 * k)
+                var pointW = new Point(w / numW * (i+1) * (w/h), h / numH * (j)* (h/w), l / numL /2 * (k))
+                var pointH = new Point(w / numW * (i) * (w/h), h / numH * (j+1)* (h/w), l / numL /2 * (k))
+                var pointL = new Point(w / numW * (i) * (w/h), h / numH * (j)* (h/w), l / numL / 2 * (k+1))
+
+                c.lineWidth = 0.25;
+
                 c.beginPath();
-                c.arc(computeX(point), computeY(point), 2 * (1-(k/2/numL)**2), 0, 2 * Math.PI);
+                c.arc(computeX(pointActual), computeY(pointActual), 2 * (1-(k/2/numL)**2), 0, 2 * Math.PI);
                 c.fill();
+                if (j-1 < numW && i < numH){
+                    c.beginPath();
+                    c.moveTo(computeX(pointActual), computeY(pointActual));
+                    c.lineTo(computeX(pointW), computeY(pointW));
+                    c.stroke();
+                }
+                if (i -1< numH && j < numW) {
+                c.beginPath();
+                c.moveTo(computeX(pointActual), computeY(pointActual));
+                c.lineTo(computeX(pointH), computeY(pointH));
+                c.stroke();
+                }
+
+                if (k + 1 < 2 * numL+1 ) {
+                  c.beginPath();
+                c.moveTo(computeX(pointActual), computeY(pointActual));
+                c.lineTo(computeX(pointL), computeY(pointL));
+                c.stroke();                  
+                }
             }
         }
     }
@@ -73,53 +110,38 @@ function computeY(Point) {
 }
 c.fillRect(0, 0, w, h);
 
-function loop() {
+function draw() {
     c.fillRect(0, 0, w, h);
     drawBox();
     for (let i = 0; i < boxPoints.length; i++) {
         var pt = boxPoints[i];
         c.beginPath();
-        c.arc(computeX(pt), computeY(pt), 5 * (1.1 / pt.z), 0, 2 * Math.PI);
-        c.fillStyle = 'red';
+        c.arc(computeX(pt), computeY(pt), 1, 0, 2 * Math.PI);
+        c.fillStyle = 'white';
         c.fill();
     }
 }
-loop();
-drawPoints();
+draw();
+
+window.addEventListener('click', function(){
+    if (this.document.getElementById("showGridCheckBox").checked && !boxDrawn){
+        drawGridBox();
+        boxDrawn = true;
+    }
+    else if (!this.document.getElementById("showGridCheckBox").checked){
+        c.fillStyle = 'black';
+        draw();
+        boxDrawn = false;
+    }
+})
 
 window.addEventListener('resize', function() {
-    canvas.width = Math.floor(this.innerWidth / rMax) * rMax;
-    canvas.height = Math.floor(this.innerHeight / rMax) * rMax;
-    h = canvas.height;
-    w = canvas.width;
-    l = Math.min(h, w);
-    
-    boxPoints = [
-        new Point(-w, -h, 0),
-        new Point(w, -h, 0),
-        new Point(w, h, 0),
-        new Point(-w, h, 0),
-        new Point(-w, -h, l),
-        new Point(w, -h, l),
-        new Point(w, h, l),
-        new Point(-w, h, l),
-    ];
-
-    boxEdges = [
-        [0, 1], [1, 2], [2, 3], [3, 0],
-        [0, 4], [1, 5], [2, 6], [3, 7],
-        [4, 5], [5, 6], [6, 7], [7, 4]
-    ];
-    loop();
+    reDim();
+    updateBox();
+    draw();
     numW = w / rMax;
     numH = h / rMax;
     numL = Math.min(numH, numW);
-    drawPoints();
+    drawGridBox();
 })
 
-// var box [
-//     [1];
-//     , [], [], [],
-//     [], [], [], [],
-//     [], [], [], []
-// ];
