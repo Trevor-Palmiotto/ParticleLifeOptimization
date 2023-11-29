d2dd35dac79dafe15cd2548497c084dd57a4a7cb
Trevor-Palmiotto <ssztrevor@gmail.com>
2023-11-28 20:36:21 -0500

    One step closer to lattice partitioning, cube now showing in lattice, still a few oddities like line drawing over/under the cube

--- Draw3D.js
+++ Draw3D.js
@@ -2,6 +2,10 @@ var slider = document.getElementById('slider');
 var output = document.getElementById('val');
 slider.oninput = function() {
     output.innerHTML = this.value;
+    c.fillStyle = 'black';
+    c.fillRect(0,0,w_, h_);
+    drawBox();
+    drawLattice(this.value);
 }
 var canvas = document.getElementById('myCanvas');
 const rMax = 200;
@@ -37,12 +41,153 @@ console.log(numH);
 console.log(numL);
 
 // Slider info
-slider.max = w_ * h_ * l_ / (rMax*rMax*rMax);
+slider.max = w_ * h_ * l_ / (rMax*rMax*rMax) - 1;
 slider.value = slider.max / 2;
 output.innerHTML = slider.value;
+const Point = function(x, y, z) {this.x = x; this.y = y; this.z = z / Math.min(h_, w_) + d};
+// n is a 1D version of the 3D grid
+function fillPartition(n) {
+    let length = Math.floor(n / (numW * numH));
+    let height = Math.floor((n % (numW * numH)) / numW);
+    let width = n % numW;
+    // console.log(length);
+    // console.log(height);
+    // console.log(width);
+    let Ls = [
+        ((length - 1) + numL) % numL, 
+        ((length) + numL) % numL,
+        ((length + 1) + numL) % numL
+    ];
+    let Hs = [
+        ((height - 1) + numH) % numH,
+        ((height) + numH) % numH,
+        ((height + 1) + numH) % numH
+    ];
+    let Ws = [
+        ((width - 1) + numW) % numW,
+        ((width) + numW) % numW,
+        ((width + 1) + numW) % numW
+    ];
+    // console.log(Ls);
+    // console.log(Hs);
+    // console.log(Ws);
+}
+function cubeTop(p, length) {
+    let pt = Object.create(p);
+    // drawing top face
+    c.fillStyle = "purple"
+    c.beginPath();  
+    c.moveTo(computeX(pt), computeY(pt));
+    pt.z = (length * rMax + rMax) / Math.min(h_, w_) + d;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.x += rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.z = (length * rMax) / Math.min(h_, w_) + d;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.x -= rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    c.fill();
+
+}
+function cubeBottom(p, length) {
+    let pt = Object.create(p);
+    // drawing bottom face
+    c.fillStyle = "purple"
+    pt.y += rMax;
+    c.beginPath();  
+    c.moveTo(computeX(pt), computeY(pt));
+    pt.z = (length * rMax + rMax) / Math.min(h_, w_) + d;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.x += rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.z = (length * rMax) / Math.min(h_, w_) + d;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.x -= rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    c.fill();
+}
+function cubeLeft(p, length) {
+    let pt = Object.create(p);
+    // // drawing left face
+    c.fillStyle = "green"
+    c.beginPath();  
+    c.moveTo(computeX(pt), computeY(pt));
+    pt.z = (length * rMax + rMax) / Math.min(h_, w_) + d;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.y += rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.z = (length * rMax) / Math.min(h_, w_) + d;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.y -= rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    c.fill();
+
+}
+function cubeRight(p, length) {
+    let pt = Object.create(p);
+    // drawing right face
+    c.fillStyle = "green"
+    pt.x += rMax;
+    c.beginPath();  
+    c.moveTo(computeX(pt), computeY(pt));
+    pt.z = (length * rMax + rMax) / Math.min(h_, w_) + d;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.y += rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.z = (length * rMax) / Math.min(h_, w_) + d;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.y -= rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    c.fill();
+}
+function cubeFront(p) {
+    let pt = Object.create(p);
+    // drawing front face  
+    c.fillStyle = "blue";
+    c.beginPath();
+    c.lineWidth = 0;
+    c.moveTo(computeX(pt), computeY(pt));
+    pt.x += rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.y += rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.x -= rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    pt.y -= rMax;
+    c.lineTo(computeX(pt), computeY(pt));
+    c.fill();
+}
+function drawCube(n) {
+    let length = Math.floor(n / (numW * numH));
+    let height = Math.floor((n % (numW * numH)) / numW);
+    let width = n % numW;
+    let pt = new Point(width*rMax, height*rMax, length*rMax);
+    if (width < (numW - 1) / 2) {
+        cubeLeft(pt, length);
+        if (height < (numH - 1) / 2) {
+            cubeBottom(pt, length);
+        } else {
+            cubeTop(pt, length);
+        }
+        cubeRight(pt, length);
+        cubeFront(pt);
+    } else if (width == (numW - 1) / 2) {
+        cubeBottom(pt, length);
+        cubeTop(pt, length);
+        cubeFront(pt);
+    } else {
+        cubeRight(pt, length);
+        if (height < (numH - 1) / 2) {
+            cubeBottom(pt, length);
+        } else {
+            cubeTop(pt, length);
+        }
+        cubeLeft(pt, length);
+        cubeFront(pt);
+    }
+}
 
 // point class
-const Point = function(x, y, z) {this.x = x; this.y = y; this.z = z / Math.min(h_, w_) + d};
 var boxPoints = [];
 var boxEdges = [];
 // updates box information
@@ -76,22 +221,33 @@ function drawBox() {
     }
 }
 // draws lattice
-function drawLattice() {
+function drawLattice(n) {
     c.strokeStyle = 'red';
     c.fillStyle = 'red';    
     c.lineWidth = 0.5;
-    for (let j = 0; j < numW + 1; j++) {
-        for (let i = 0; i < numH + 1; i++) {
-            for (let k = 0; k < numL + 1; k++) {
+    for (let k = numL; k > -1; k--) {
+        for (let i = numH; i > -1; i--) {
+            for (let j = numW; j > -1; j--) {
                 var pointActual = new Point(w_ / numW * j, h_ / numH * i, l_ / numL * k)
+                var pointW_minus = new Point(w_ / numW * (j-1), h_ / numH * i, l_ / numL * k)
                 var pointW = new Point(w_ / numW * (j+1), h_ / numH * i, l_ / numL * k)
                 var pointH = new Point(w_ / numW * j, h_ / numH * (i+1), l_ / numL * k)
                 var pointL = new Point(w_ / numW * j, h_ / numH * i, l_ / numL * (k+1))
-                // Drawing point - size scales based on z axis
-                c.beginPath();
-                c.arc(computeX(pointActual), computeY(pointActual), 2 * (1-(k/2/numL)**2), 0, 2 * Math.PI);
-                c.fill();
-                // draw horizontal lines
+                c.strokeStyle = 'red';
+                c.fillStyle = 'red';   
+                c.lineWidth = 0.5;
+                if (k < numL) {
+                    c.beginPath();
+                    c.moveTo(computeX(pointActual), computeY(pointActual));
+                    c.lineTo(computeX(pointL), computeY(pointL));
+                    c.stroke();
+                }
+                if (j + i * numW + k * numW * numH == n) {
+                    drawCube(n);
+                }
+                c.strokeStyle = 'red';
+                c.fillStyle = 'red';    
+                c.lineWidth = 0.5;
                 if (j < numW){
                     c.beginPath();
                     c.moveTo(computeX(pointActual), computeY(pointActual));
@@ -106,12 +262,15 @@ function drawLattice() {
                     c.stroke();
                 }
                 // draw extruding lines
-                if (k < numL) {
-                    c.beginPath();
-                    c.moveTo(computeX(pointActual), computeY(pointActual));
-                    c.lineTo(computeX(pointL), computeY(pointL));
-                    c.stroke();
-                }
+
+
+
+                // Drawing point - size scales based on z axis
+                c.beginPath();
+                c.arc(computeX(pointActual), computeY(pointActual), 2 * (1-(k/2/numL)**2), 0, 2 * Math.PI);
+                c.fill();
+                // draw horizontal lines
+
             }
         }
     }
@@ -141,7 +300,7 @@ draw();
 // draws lattice when checkbox is checked
 window.addEventListener('click', function(){
     if (this.document.getElementById("showLatticeCheckBox").checked && !boxDrawn){
-        drawLattice();
+        drawLattice(slider.value);
         boxDrawn = true;
     }
     else if (!this.document.getElementById("showLatticeCheckBox").checked){
@@ -159,6 +318,9 @@ window.addEventListener('resize', function() {
     numH = h_ / rMax;
     numL = Math.min(numH, numW);
     if(boxDrawn){
-        drawLattice();
+        drawLattice(slider.value);
     }
 })
+// drawCube(2);
+
+// fillPartition(95);
